34558355c70b644eb07a76ffd1e31cf7946c2e96
Oleh Pashyn <oleh.pashyn@tobii.com>
2020-01-22 11:29:23 +0200

    Day4 added

--- AdventOfCode/Launcher/Launcher.cs
+++ AdventOfCode/Launcher/Launcher.cs
@@ -11,7 +11,7 @@ namespace Launcher
             {"1", new Day1()},
             {"2", new Day2()},
             {"3", new Day3()},
-            //{"4", new Day4()},
+            {"4", new Day4()},
             //{"5", new Day5()},
             //{"6", new Day6()},
             //{"7", new Day7()},
---
+++ Year2019/Day4.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Year2019
+{
+    public class Day4 : TaskDay
+    {
+        private readonly int _start = int.Parse(File.ReadAllText(InputFileDirectory + "Day4.txt").Split('-')[0]);
+        private readonly int _end = int.Parse(File.ReadAllText(InputFileDirectory + "Day4.txt").Split('-')[1]);
+
+        public override string Task1()
+        {
+            var count = 0;
+            for (var i = _start; i <= _end; i++)
+            {
+                var number = i.ToString().ToCharArray();
+                if ((AdjacentSame(number).Max() > 1) & NeverDecrease(number))
+                {
+                    count++;
+                }
+            }
+
+            return count.ToString();
+        }
+
+        public override string Task2()
+        {
+            var count = 0;
+            for (var i = _start; i <= _end; i++)
+            {
+                var number = i.ToString().ToCharArray();
+                if (AdjacentSame(number).Contains(2) & NeverDecrease(number))
+                {
+                    count++;
+                }
+            }
+
+            return count.ToString();
+        }
+
+
+        private static IEnumerable<int> AdjacentSame(char[] number)
+        {
+            var count = 1;
+            var counters = new List<int>();
+            for (var a = 0; a < 5; a++)
+            {
+                if (number[a] == number[a + 1])
+                {
+                    count++;     
+                }
+                else
+                {
+                    counters.Add(count);
+                    count = 1;
+                }
+            }
+            counters.Add(count);
+
+            return counters;
+        }
+
+        private static bool NeverDecrease(IReadOnlyList<char> number)
+        {
+            var flag = true;
+            for (var a = 0; a < 5; a++)
+            {
+                if (number[a] > number[a + 1])
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+    }
+}
